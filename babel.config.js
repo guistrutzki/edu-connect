40df18e5fefdad0088f8c31c083eff775d9da503
Guillherme Strutzki <guilhermestrutzki1@natura.net>
2025-10-04 18:41:24 -0300

    feat: update app configuration, add metro bundler support, and enhance package dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = api => {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+      ],
+    ],
     plugins: [
       [
         'module-resolver',
